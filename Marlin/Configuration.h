bf5e93d49477a0aca07e6d96cfb2a19748ec2c26
Hisham Ghosheh <hisham.ghosheh@gmail.com>
2022-01-07 10:53:52 +0100

    Hardcode probe z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1182,7 +1182,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -47, -7, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -47, -7, -0.9 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
